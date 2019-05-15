74259701567f3b4911d3ff50cf94d3ce50336315
Rolando Garro <rolando@emptyart.xyz>
2019-05-14 20:54:55 -0600

    boricua baby looney tune
    boricua baby looney tune

---
+++ fig0106/fig0106.cpp
@@ -0,0 +1,25 @@
+/***
+* Suma, en el hash del commit esta el premio de la loteria ..
+*
+* @author Rolando <rgarro@gmail.com>
+*/
+
+#include <iostream>
+
+int main(){
+	int entero1;
+	int entero2;
+	int suma;
+	
+	std::cout << "Digite el primer entero \n";
+	std::cin >> entero1;
+
+	std::cout << "Digite el segundo entero \n";
+	std::cin >> entero2;
+
+	suma = entero1 + entero2;
+
+	std::cout << "la suma es " << suma << std::endl;
+
+	return 0;
+}
