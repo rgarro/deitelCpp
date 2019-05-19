a2764670b38a22ef43c9503de72f231ec7f865ae
Rolando Garro <rolando@emptyart.xyz>
2019-05-19 16:26:59 -0600

    where I lay my head is home my body lies but still roam
    where I lay my head is home my body lies but still roam

---
+++ fig214/main.cpp
@@ -0,0 +1,21 @@
+//incremento y posincremento
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(){
+	int c;
+	c = 5;
+	cout << c << endl;
+	cout << c++ << endl;
+	cout << c << endl << endl;
+
+	c = 5;
+	cout << c << endl;
+	cout << ++c << endl;
+	cout << c << endl;
+
+	return 0;
+}//place the enterteiner chords here ...
