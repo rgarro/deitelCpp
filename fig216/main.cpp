f41c327fb262528c686c7ac90a17b66e3579a9a4
Rolando Garro <rolando@emptyart.xyz>
2019-05-20 16:16:35 -0600

    rapero desperado es nuestro tema tequila sunrise ...
    rapero desperado es nuestro tema tequila sunrise...

---
+++ fig216/main.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+int main(){
+	int counter = 1;
+	int max = 10;
+
+	while(counter <= max){
+		cout << counter << endl;
+		counter += 1;
+	}
+	
+	return 0;
+}// enterteiner chords here ...
