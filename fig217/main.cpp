e6606c72dfb5674bd4bad5a60f7196b6e9923e6e
Rolando Garro <rolando@emptyart.xyz>
2019-05-21 10:41:15 -0600

    the fucking enterteiner chords ...
    the fucking enterteiner chords ...

---
+++ fig217/main.cpp
@@ -0,0 +1,20 @@
+/**
+ * shadow mirages, metabo clockwise sparks fell , fm dying waves ...
+ *
+ * @author <Rolando> rgarro@gmail.com
+ */
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(){
+	int max_loops = 10;
+	int start_at = 1;
+
+	for(int loops = start_at;loops <= max_loops;loops++){
+		cout << loops << endl;
+	}
+
+	return 0;
+}//D3Db3 E3C4 E3C4 C4D4Db4E4 C4D4E4 B3D4C4 ....
