6c00f93c922637c4018466b211feb312502a140c
Rolando Garro <rolando@emptyart.xyz>
2019-06-10 16:29:02 -0600

    lcd quartz timing death ...
    lcd quartz timing death ...

--- antBunker/fig411/main.cpp
+++ antBunker/fig411/main.cpp
@@ -10,10 +10,16 @@ int main(){
 	const int frecuencySize = 11;
 
 	int answers[answerSize] = {1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10};
-	int frecuency[frecuencySize] = (0);
+	int frecuency[frecuencySize] = {0};
 
 	for(int answer = 1; answer < frecuencySize; answer+=1){
 		++frecuency[answers[answer]];
+	}
+
+	cout << "Calificacion: " << setw(17) << "Frecuency: " << endl;
+
+	for(int calificacion = 1; calificacion < frecuencySize;calificacion+=1){
+		cout << setw(6) << calificacion << setw(17) << frecuency[calificacion] << endl;
 	}
 
 	return 0;
