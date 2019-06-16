ae878af36a7c5b9933980062d24b7bd2c6173496
Rolando Garro <rolando@emptyart.xyz>
2019-06-16 15:17:40 -0600

    this. will continue ...
    this. will continue ..

---
+++ antBunker/fig515/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+
+using std::cout;
+using std::endl;
+using std::setw;
+
+void sortBubble(int *,const int);
+void interchange(int * const,int * const);
+
+int main(){
+	const int arraySize = 10;
+	int a[arraySize] = {2,4,5,7,8,12,45,34,23,56};
+
+	return 0;
+}//D3Db3 E3C4 E3C4 C4D4Db4E4 C4D4E4 B3D4C4 ....
