7542548bc77db1074b236323352e762e82a0bf6f
Rolando Garro <rolando@emptyart.xyz>
2019-06-20 07:22:57 -0600

    charBurger #1 San Joaquin de Heredia ....
    charBurger #1 San Joaquin de Heredia ....

---
+++ antBunker/fig532/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+
+using std::cout;
+using std::endl;
+
+int main(){
+	char *cadena1 = "abcdefghijklmnopqrstuvwxyz";//charBurger#1 San Joaquin de Heredia ...
+	char *cadena2 = "cuatro";
+	char *cadena3 = "Mexico";
+
+	cout << "#1 Len: " << strlen(cadena1) << "-" << strlen(cadena2) << "-" << strlen(cadena3) << endl; 
+
+	return 0;
+}//D3Db3 E3C4 E3C4 C4D4Db4E4 C4D4E4 B3D4C4 ....
+
