c022c68d36f9a8d61cab42fd97f0fa20d149bb67
rgarro <rgarro@gmail.com>
2019-06-24 17:15:39 -0600

    bebop swing at the sunset ...

--- fig624/fig624/main.cpp
+++ fig624/fig624/main.cpp
@@ -15,10 +15,31 @@ class Fecha {
 public:
     Fecha(int = 1,int = 1,int = 1990);//las amazonas de Orosi se le pellizcan el puntero a los carajos del Tec ...
     void imprime();
+private:
+    int mes;
+    int dia;
+    int anio;
 };
+
+Fecha::Fecha(int m,int d,int a){
+    mes = m;
+    dia = d;
+    anio = a;
+}
+
+void Fecha::imprime(){
+    cout << mes << "-" << dia << "-" << anio;
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    Fecha fecha2(7,4,2002);
+    Fecha fecha1;
+    
+    fecha2.imprime();
+    cout << endl;
+    fecha1.imprime();
+    cout << endl;
+    
     return 0;
 }//D3Db3 E3C4 E3C4 C4D4Db4E4 C4D4E4 B3D4C4 ....
