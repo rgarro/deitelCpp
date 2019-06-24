4ff459a66f1d9e4cc493ad17ef065912b4f42f72
rgarro <rgarro@gmail.com>
2019-06-24 16:22:16 -0600

    kind of blue up in the sky ...

---
+++ fig624/fig624/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  fig624
+//
+//  Created by Rolando Garro on 6/24/19.
+//  Copyright © 2019 Rolando Garro. All rights reserved.
+//
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+class Fecha {
+public:
+    Fecha(int = 1,int = 1,int = 1990);//las amazonas de Orosi se le pellizcan el puntero a los carajos del Tec ...
+    void imprime();
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}//D3Db3 E3C4 E3C4 C4D4Db4E4 C4D4E4 B3D4C4 ....
