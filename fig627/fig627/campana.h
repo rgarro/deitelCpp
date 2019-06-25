0a8fdfd52517855ea4d243d957fcf31ffd15716b
rgarro <rgarro@gmail.com>
2019-06-25 13:55:09 -0600

    conjuring the dakinis in a carrizal cremation ground ...

---
+++ fig627/fig627/campana.h
@@ -0,0 +1,13 @@
+//
+//  campana.h
+//  fig627
+//
+//  Created by Rolando Garro on 6/25/19.
+//  Copyright © 2019 Rolando Garro. All rights reserved.
+//
+
+#ifndef campana_h
+#define campana_h
+
+
+#endif /* campana_h */
---
+++ fig627/fig627/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  fig627
+//
+//  Created by Rolando Garro on 6/25/19.
+//  Copyright © 2019 Rolando Garro. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}//D3Db3 E3C4 E3C4 C4D4Db4E4 C4D4E4 B3D4C4 ....
